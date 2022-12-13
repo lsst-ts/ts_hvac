ec48ceb12aa8fff5c90e9fb25d42993a6b61da78
Wouter van Reeven <wvanreeven@lsst.org>
2022-12-13 16:25:43 +0100

    Improve handling of payloads that cannot be decoded by JSON.

--- python/lsst/ts/hvac/csc.py
+++ python/lsst/ts/hvac/csc.py
@@ -59,6 +59,11 @@ TOPICS_WITHOUT_COMANDO_ENCENDIDO = frozenset(
     )
 )
 
+# These strings cannot be decoded by JSON and need to be treated separately.
+STRINGS_THAT_CANNOT_BE_DECODED_BY_JSON = {
+    b"AUTOMATICO {ok} @ 10",
+}
+
 
 def run_hvac() -> None:
     asyncio.run(HvacCsc.amain(index=None))
@@ -397,14 +402,17 @@ class HvacCsc(salobj.BaseCsc):
         while not len(self.mqtt_client.msgs) == 0:
             msg = self.mqtt_client.msgs.popleft()
             topic_and_item = msg.topic
-            try:
-                payload = json.loads(msg.payload)
-            except json.decoder.JSONDecodeError:
-                self.log.error(
-                    f"Exception decoding topic {msg.topic} "
-                    f"payload {msg.payload}. Continuing."
-                )
-                continue
+            if msg.payload in STRINGS_THAT_CANNOT_BE_DECODED_BY_JSON:
+                payload = msg.payload.decode("utf-8")
+            else:
+                try:
+                    payload = json.loads(msg.payload)
+                except json.decoder.JSONDecodeError:
+                    self.log.error(
+                        f"Exception decoding topic {msg.topic} "
+                        f"payload {msg.payload}. Continuing."
+                    )
+                    continue
 
             topic, item = self.xml.extract_topic_and_item(topic_and_item)
             topic = re.sub(r"PISO([1-9])", r"PISO0\1", topic)
@@ -442,7 +450,6 @@ class HvacCsc(salobj.BaseCsc):
                 await asyncio.sleep(HVAC_STATE_TRACK_PERIOD)
         except asyncio.CancelledError:
             # Normal exit
-            self.log.exception("CancelledError but this was expected.")
             pass
         except Exception as e:
             self.log.exception("Exception and this was unexpected.")
